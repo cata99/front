db9c90342987a90e26e749ba42640f7d6700c5e8
cata99 <catupaschini@hotmail.com>
2022-09-22 18:55:22 -0300

    change menu again

--- src/components/Layout/Menu.js
+++ src/components/Layout/Menu.js
@@ -4,10 +4,20 @@ import { Link } from "react-router-dom";
 function Menu() {
   return (
     <nav>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <ul className="menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movements">Movimientos</Link>
+        </li>
+        <li>
+          <Link to="/institutions">Institutiones</Link>
+        </li>
+        <li>
+          <Link to="/users">Usuarios</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
